fe74f9e0e6d96f728692208a2d241c8dada99a18
Yi EungJun <semtlenori@gmail.com>
2015-09-13 23:07:46 +0900

    success
    Fix the last error by adding @Entity annotation

--- src/main/java/eungjun/Person.java
+++ src/main/java/eungjun/Person.java
@@ -1,8 +1,10 @@
 package eungjun;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class Person {
     @GeneratedValue
     @Id
