4045468d46f9808eeae688167fd3b88a9a4bc976
Yi EungJun <semtlenori@gmail.com>
2015-09-13 23:23:15 +0900

    Fix the error by adding equals and hashCode
    We consider only the id.

--- src/main/java/eungjun/Person.java
+++ src/main/java/eungjun/Person.java
@@ -19,4 +19,20 @@ public class Person {
     public String getName() {
         return name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return !(id != null ? !id.equals(person.id) : person.id != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
