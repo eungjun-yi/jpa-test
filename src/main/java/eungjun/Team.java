3bc71e41bff4b99e2858e9420be59f8414780ec5
Yi EungJun <semtlenori@gmail.com>
2015-09-13 23:50:28 +0900

    wip: team

---
+++ src/main/java/eungjun/Team.java
@@ -0,0 +1,37 @@
+package eungjun;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Yi EungJun <eungjun.yi@gmail.com>
+ */
+@Entity
+public class Team {
+
+    @GeneratedValue
+    @Id
+    Long id;
+
+    private String name;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<Membership> members = new HashSet<>();
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Membership> getMembers() {
+        return members;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
