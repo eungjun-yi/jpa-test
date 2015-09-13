03c544d6540f5b7b907d2c7078421f600f593382
Yi EungJun <semtlenori@gmail.com>
2015-09-13 22:58:44 +0900

    another error
    Caused by: java.io.FileNotFoundException: class path resource [org/springframework/security/config/annotation/web/configuration/WebSecurityConfigurerAdapter.class] cannot be opened because it does not exist

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationContextLoader;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = SpringApplicationContextLoader.class, classes = Application.class)
+public class PersonTest {
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    public void create() {
+        Person person = new Person();
+        person.setName("yi");
+        personRepository.save(person);
+        person = personRepository.findByName("yi");
+        assertThat(person.getName(), is("yi"));
+    }
+}
